7941fc08d5f5e230e1b0a028d5a6db1b350e6318
Amanda Braun <Ac_br_aun@hotmail.com>
2017-01-06 09:20:56 -0600

    initial commit

---
+++ src/simplechat/ChatIF.java
@@ -0,0 +1,15 @@
+package simplechat;
+
+
+
+
+
+
+public interface ChatIF 
+{
+  /**
+   * Method that when overriden is used to display objects onto
+   * a UI.
+   */
+  public abstract void display(String message);
+}
